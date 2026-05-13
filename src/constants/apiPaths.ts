9bc9fae04a35f20d664b9569d17458a8090f0239
Igor <igopood33@gmail.com>
2026-05-13 23:23:57 +0200

    added import service link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: import.meta.env.PRODUCT_SERVICE_API_URL,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: import.meta.env.IMPORT_SERVICE_API_URL,
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
