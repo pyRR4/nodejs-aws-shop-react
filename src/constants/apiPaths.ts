ec016bcb99bf0ba69f693c467e8eb9e78138af89
Igor <igopood33@gmail.com>
2026-05-15 21:39:53 +0200

    fixed api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://o9glapsl74.execute-api.eu-central-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://haoibow83h.execute-api.eu-central-1.amazonaws.com/prod/",
+  import: "https://haoibow83h.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
